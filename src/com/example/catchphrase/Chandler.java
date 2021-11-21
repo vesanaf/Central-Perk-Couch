58495a151a4866c25e6f085c5f48aa9e68fd340b
Ross Geller <happyonsky@yandex.ru>
2021-11-21 20:38:29 +0300

    CPC-T-7 fixed the emphasis *eye roll*

--- src/com/example/catchphrase/Chandler.java
+++ src/com/example/catchphrase/Chandler.java
@@ -2,6 +2,6 @@ package com.example.catchphrase;
 
 public class Chandler {
     public static void main(String[] args) {
-        System.out.println("Could it be more obvious?!");
+        System.out.println("Could it BE more obvious?!");
     }
 }
